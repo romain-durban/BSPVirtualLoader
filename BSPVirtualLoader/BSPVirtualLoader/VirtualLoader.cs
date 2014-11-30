4098c5c6674c493ba69346d3f440ec10aed2d108
Sylver <romain.durban@gmail.com>
2014-11-30 21:19:50 +0100

    Added BSP header loading
    ADD: Loads the header of a BSP file.

---
+++ BSPVirtualLoader/BSPVirtualLoader/VirtualLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace BSPVirtualLoader
+{
+	public class VirtualLoader
+	{
+
+		public BSP.BSPLoader bsploader {
+			get;
+			private set;
+		}
+		public VirtualLoader (String filename)
+		{
+			FileStream fileStream = File.OpenRead(filename);
+			BinaryReader br = new BinaryReader(fileStream);
+			bsploader = new BSP.BSPLoader ();
+			bsploader.LoadBSP (br);
+		}
+	}
+}
+
